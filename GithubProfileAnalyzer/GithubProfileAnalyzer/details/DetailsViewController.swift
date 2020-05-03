ab909315c7da40346987f752d7a43ddeda0259ca
Haydn Wiese <haydnwiese@gmail.com>
2020-05-03 13:02:13 -0400

    Created test layout for charts

--- GithubProfileAnalyzer/GithubProfileAnalyzer/details/DetailsViewController.swift
+++ GithubProfileAnalyzer/GithubProfileAnalyzer/details/DetailsViewController.swift
@@ -7,12 +7,14 @@
 //
 
 import UIKit
+import Charts
 
 class DetailsViewController: UIViewController {
     @IBOutlet weak var profilePictureImageView: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var organizationLabel: UILabel!
     @IBOutlet weak var repoCountLabel: UILabel!
+    @IBOutlet weak var repoLanguageChartView: PieChartView!
     
     var userDetails: (SearchResult, UserDetails)?
     var profilePicture: UIImage?
@@ -23,6 +25,10 @@ class DetailsViewController: UIViewController {
         
         updateUserDetails()
         fetchRepos()
+        
+        let players = ["Ozil", "Ramsey", "Laca", "Auba", "Xhaka", "Torreira"]
+        let goals = [6, 2, 26, 30, 8, 10]
+        createRepoLanguageChart(dataPoints: players, values: goals.map{ Double($0) })
     }
     
     // MARK: Private Methods
@@ -44,5 +50,38 @@ class DetailsViewController: UIViewController {
                 self.repos += response
             }
         }
+    }
+    
+    private func createRepoLanguageChart(dataPoints: [String], values: [Double]) {
+        var dataEntries = [ChartDataEntry]()
+        for (i,label) in dataPoints.enumerated() {
+            let dataEntry = PieChartDataEntry(value: values[i], label: label, data: label as AnyObject)
+            dataEntries.append(dataEntry)
+        }
+        
+        let pieChartDataSet = PieChartDataSet(entries: dataEntries, label: nil)
+        pieChartDataSet.colors = colorsOfCharts(numbersOfColor: dataPoints.count)
+
+        let pieChartData = PieChartData(dataSet: pieChartDataSet)
+        let format = NumberFormatter()
+        format.numberStyle = .none
+        let formatter = DefaultValueFormatter(formatter: format)
+        pieChartData.setValueFormatter(formatter)
+        
+        repoLanguageChartView.legend.enabled = false
+        repoLanguageChartView.holeColor = nil
+        repoLanguageChartView.data = pieChartData
+    }
+    
+    private func colorsOfCharts(numbersOfColor: Int) -> [UIColor] {
+      var colors: [UIColor] = []
+      for _ in 0..<numbersOfColor {
+        let red = Double(arc4random_uniform(256))
+        let green = Double(arc4random_uniform(256))
+        let blue = Double(arc4random_uniform(256))
+        let color = UIColor(red: CGFloat(red/255), green: CGFloat(green/255), blue: CGFloat(blue/255), alpha: 1)
+        colors.append(color)
+      }
+      return colors
     }
 }
